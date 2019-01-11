bac188729f6c428d474352b6fe5489e7ef82406d
Steven Watanabe <watanabe@battelle.org>
2019-01-10 17:29:23 -0800

    Cleaning up the code a bit.

--- src/test/java/generalTesting/GoodTestCases.java
+++ src/test/java/generalTesting/GoodTestCases.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.Properties;
 import mil.dtic.sitemaps.management.SitemapManagerApplication;
 import mil.dtic.sitemaps.management.configuration.SitemapManagerConfiguration;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
@@ -16,29 +14,23 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Rule;
-import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-/**
- *
- */
 @SpringBootTest(classes = SitemapManagerApplication.class)
 //@RunWith(SpringJUnit4ClassRunner.class)
 @RunWith(SpringRunner.class)
@@ -46,8 +38,6 @@ import org.springframework.web.context.WebApplicationContext;
 @AutoConfigureMockMvc
 //@WebMvcTest
 //@WebAppConfiguration
-//@ContextConfiguration("my-servlet-context.xml")
-//@TestPropertySource(locations = "classpath:../../resources/test-application.properties")
 @TestPropertySource(locations = "classpath:test-application.properties")
 public class GoodTestCases {
 
@@ -73,10 +63,6 @@ public class GoodTestCases {
 	@Before
 	public void setUp() throws IOException {
 		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-		//Properties props = System.getProperties();
-		//props.setProperty("java.io.tmpdir", config.getRootPath());
-		//testFolder.newFile("touch.txt");
-		//FileUtils.cleanDirectory(config.getRootPath());
 		Files.createDirectories(Paths.get(config.getRootPath()));
 	}
 	
@@ -88,16 +74,8 @@ public class GoodTestCases {
 	@Test
 	public void testPostEmptyList() throws Exception {
 		mockMvc.perform(post("/sitemap-manager").content("{}").contentType(MediaType.APPLICATION_JSON).accept(MediaType.TEXT_PLAIN))
-		//mockMvc.perform(post("/sitemap-manager", "").accept(MediaType.TEXT_PLAIN))
-			//.andDo(print())
 			.andExpect(status().isCreated())
 			.andExpect(content().contentType("text/plain;charset=UTF-8"))
-			//.andExpect(jsonPath("$.timestamp").value(String.valueOf(Instant.now().getEpochSecond())))
-			//.andExpect(jsonPath("$.status").value("400"))
-			//.andExpect(jsonPath("$.error").value("Bad Request"))
-			//.andExpect(jsonPath("$.exception").value("org.springframework.http.converter.HttpMessageNotReadableException"))
-			//.andExpect(jsonPath("$.message").value("Required request body is missing: public org.springframework.http.ResponseEntity<java.lang.String> mil.dtic.sitemaps.management.controller.SitemapManagerController.addLocations(mil.dtic.sitemaps.management.resources.IndexedLocationList)"))
-			//.andExpect(jsonPath("$.path").value("/sitemap-manager"))
 			.andExpect(content().string("created"))
 			;
 
