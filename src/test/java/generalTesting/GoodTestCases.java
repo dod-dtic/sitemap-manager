4f3317e3c6ac9c60881e59600e32f0ec84d59d81
Steven Watanabe <watanabe@battelle.org>
2019-01-11 15:30:13 -0800

    Needing to change how tests work in a bunch of ways.

--- src/test/java/generalTesting/GoodTestCases.java
+++ src/test/java/generalTesting/GoodTestCases.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import mil.dtic.sitemaps.management.SitemapManagerApplication;
 import mil.dtic.sitemaps.management.configuration.SitemapManagerConfiguration;
 import mil.dtic.sitemaps.management.resources.domain.TUrl;
@@ -53,7 +54,7 @@ public class GoodTestCases {
 	@Autowired
 	private SitemapManagerConfiguration config;
 	private String sitemapIndexFile;
-	private String sitemapFile;
+	private String defaultSitemapName;
 	
 	public GoodTestCases() {
 	}
@@ -69,7 +70,7 @@ public class GoodTestCases {
 	@Before
 	public void setUp() throws IOException {
 		sitemapIndexFile = config.getRootPath() + "sitemap.xml";
-		sitemapFile = config.getRootPath() + "sitemap-AD100x.xml";
+		defaultSitemapName = config.getRootPath() + "sitemap-AD100x.xml";
 		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 		Files.createDirectories(Paths.get(config.getRootPath()));
 	}
@@ -95,34 +96,45 @@ public class GoodTestCases {
 	 * @param expectedResourcePath
 	 * @param actualXmlPath 
 	 */
-	private void compareXmlFiles(String expectedResourcePath, String actualXmlPath) throws IOException {
-		/*
-		String correctXmlString = resourceToString(expectedResourcePath);
-		String createdXmlString = fileToString(actualXmlPath);
-		assertEquals(correctXmlString, createdXmlString);
-		*/
-		
-		File expectedXmlFile = new ClassPathResource(expectedResourcePath).getFile();
-		File actualXmlFile = new File(actualXmlPath);
-		if(expectedXmlFile.exists() && !expectedXmlFile.isDirectory() && actualXmlFile.exists() && !actualXmlFile.isDirectory()) {
-			XmlMapper mapper = new XmlMapper();
-			mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-			mapper.setDateFormat(new StdDateFormat());
-			Urlset expectedUrlset = mapper.readValue(expectedXmlFile, Urlset.class);
-			Urlset actualUrlset = mapper.readValue(actualXmlFile, Urlset.class);
+	private void compareTUrls(TUrl expectedTUrl, TUrl actualTUrl) throws IOException {
+		assertEquals(expectedTUrl.getLoc(), actualTUrl.getLoc());
 
-			TUrl expectedTUrl = expectedUrlset.getUrl().get(0);
-			TUrl actualTUrl = actualUrlset.getUrl().get(0);
-
-			//assertEquals(expectedTUrl.getLastmod().getTime(), actualTUrl.getLastmod().getTime(), 1);// 60000
-			assertEquals(expectedTUrl.getLoc(), actualTUrl.getLoc());
+		if (expectedTUrl.getLastmod() != null) {
+			assertEquals(expectedTUrl.getLastmod(), actualTUrl.getLastmod());
 		} else {
-			fail("Didn't find XML files correctly.");
+			// Need to figure this out. Get current time and check within some delta. w:w
+		}
+
+		if (expectedTUrl.getChangefreq() != null) {
+			assertEquals(expectedTUrl.getLastmod(), actualTUrl.getLastmod());
+		} else {
+			assertEquals(expectedTUrl.getChangefreq(), config.getDefaultChangeFrequency());
+		}
+
+		if (expectedTUrl.getPriority() != null) {
+			assertEquals(expectedTUrl.getLastmod(), actualTUrl.getLastmod());
+		} else {
+			assertEquals(expectedTUrl.getPriority(), config.getDefaultPriority());
 		}
 	}
 
+	private List<TUrl> parseXmlFile(File xmlFile) throws IOException {
+		Urlset urlset = null;
+		if(xmlFile.exists() && !xmlFile.isDirectory()) {
+			XmlMapper mapper = new XmlMapper();
+			mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+			mapper.setDateFormat(new StdDateFormat());
+			urlset = mapper.readValue(xmlFile, Urlset.class);
+		} else {
+			fail("Didn't find XML files correctly.");
+		}
+
+		return urlset.getUrl();
+	}
+
 	/**
-	 * 
+	 * TODO need to figure out how to test file names.
+	 * TODO need to test the sitemap index
 	 * @param requestJsonPath Should be the resource path rather than full paths
 	 * @param expectedSitemapPath resource path
 	 * @param actualSitemapPath Full path
@@ -138,7 +150,9 @@ public class GoodTestCases {
 			.andExpect(content().contentType("text/plain;charset=UTF-8"))
 			.andExpect(content().string("created"));
 
-		compareXmlFiles(expectedSitemapPath, actualSitemapPath);
+		List<TUrl> expectedTUrls = parseXmlFile(new ClassPathResource(expectedSitemapPath).getFile());
+		List<TUrl> actualTUrls = parseXmlFile(new File(actualSitemapPath));
+		compareTUrls(expectedTUrls.get(0), actualTUrls.get(0));
 	}
 
 	@Test
@@ -148,22 +162,6 @@ public class GoodTestCases {
 
 	@Test
 	public void testPostOneValidEntry() throws Exception {
-		/*
-		String locationJson = resourceToString("requestJson/justLocation.json");
-
-		MockHttpServletRequestBuilder postRequest = post("/sitemap-manager")
-			.content(locationJson)
-			.contentType(MediaType.APPLICATION_JSON)
-			.accept(MediaType.TEXT_PLAIN);
-
-		mockMvc.perform(postRequest)
-			.andExpect(status().isCreated())
-			.andExpect(content().contentType("text/plain;charset=UTF-8"))
-			.andExpect(content().string("created"));
-
-		String createdXmlString = fileToString(sitemapFile);
-		System.out.println(createdXmlString);
-*/
 		generalTest("requestJson/justLocation.json", "goodSitemaps/justLocationSitemap.xml", sitemapFile);
 	}
 }
