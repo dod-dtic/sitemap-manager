430e35fea4d5cd2bbf4b0c596dbf129e08d419c8
Steven Watanabe <watanabe@battelle.org>
2019-01-13 12:32:35 -0800

    Adding some groundwork for the 'bad' tests.

--- src/test/java/generalTesting/BadTestCases.java
+++ src/test/java/generalTesting/BadTestCases.java
@@ -1,13 +1,45 @@
 package generalTesting;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import mil.dtic.sitemaps.management.SitemapManagerApplication;
+import mil.dtic.sitemaps.management.configuration.SitemapManagerConfiguration;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest(classes = SitemapManagerApplication.class)
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:test-application.properties")
 public class BadTestCases {
+	@Autowired
+	private WebApplicationContext wac;
+	private MockMvc mockMvc;
+	@Autowired
+	private SitemapManagerConfiguration config;
 	
 	public BadTestCases() {
 	}
@@ -21,11 +53,31 @@ public class BadTestCases {
 	}
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws IOException {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		Files.createDirectories(Paths.get(config.getRootPath()));
 	}
 	
 	@After
-	public void tearDown() {
+	public void tearDown() throws IOException {
+		FileUtils.deleteDirectory(new File(config.getRootPath()));
+	}
+
+	private String resourceToString(String resourcePath) throws IOException {
+		File correctFile = new ClassPathResource(resourcePath).getFile();
+		return new String(Files.readAllBytes(correctFile.toPath()));
+	}
+
+	private void storedJsonPostRequest(String requestJsonPath) throws Exception {
+	MockHttpServletRequestBuilder postRequest = request(HttpMethod.POST, "/sitemap-manager")
+			.content(resourceToString(requestJsonPath))
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.TEXT_PLAIN);
+
+		mockMvc.perform(postRequest)
+			.andExpect(status().isCreated())
+			.andExpect(content().contentType("text/plain;charset=UTF-8"))
+			.andExpect(content().string("created"));
 	}
 
 	// TODO add test methods here.
